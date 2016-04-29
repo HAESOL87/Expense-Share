1728f8fa013514caa17def6bd7001211534c8164
Haesol <haesol87@gmail.com>
2016-04-29 10:44:15 -0400

    Added superhero Bootswatch theme.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "Expense Splitting App"
+    base_title = "Expen$e $hare"
     if page_title.empty?
       base_title
     else
